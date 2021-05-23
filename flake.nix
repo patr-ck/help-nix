51ceea404ee7a0d7f3bc1bad95fec836f0325c44
Patrick Sauter <p@trick.codes>
2021-05-22 22:22:54 -0500

    simplified

--- flake.nix
+++ flake.nix
@@ -6,28 +6,19 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   
   outputs = { self, nixpkgs, haskellNix, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    builtins.trace haskellNix (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        overlays =
-          [ haskellNix.overlay
-            (final: prev: {
-              test = final.haskell-nix.project {
-                src = ./.;
-                compiler-nix-name = "ghc8104";
-              };
-            })
-          ];
-        pkgs  = import nixpkgs { inherit system overlays; };
-        flake = pkgs.test.flake {};
+        overlays = [ haskellNix.overlay (final: prev: {
+	  test2 = final.haskell-nix.project' {
+	    src = ./.;
+	    compiler-nix-name = "ghc8104";
+          };
+	}) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        flake = pkgs.test2.flake {};
       in
         flake // {
-          defaultPackage = flake.packages."test:exe:test";
-
-          devShell = pkgs.test.shellFor {
-            tools = {
-              cabal = "latest";
-            };
-          };
-        }
-    );
+          defaultPackage = flake.packages."test2:exe:test2";
+	}
+    ));
 }
