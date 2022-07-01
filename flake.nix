0dc38374c794c72d366bb9bf3f1869e2efcf1159
Samuel Evans-Powell <mail@sevanspowell.net>
2022-07-01 10:40:48 +0800

    Provide ghcjs shell
    - Use "crossPlatforms" haskell.nix attribute to provide a ghc-js compiler in the
    shell.
    - Overcome "Package key for wired-in dependency `ghcjs-th' could not be found:
    ghcjs-th-0.1.0.0-JdGbYrK1CstBZz9Zn9tGjt" error by using known good flake.lock.
      - Sorry I don't have more information on this yet.
    - Add instructions for using ghc-js in the shell.

--- flake.nix
+++ flake.nix
@@ -5,23 +5,34 @@
     url = "github:input-output-hk/haskell.nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-2105";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-2111";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, haskellNix, flake-utils }:
     builtins.trace haskellNix (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         overlays = [ haskellNix.overlay (final: prev: {
-        test2 = final.haskell-nix.project' {
-          src = ./.;
-          compiler-nix-name = "ghc8107";
-          modules = [
-            { packages.webkit2gtk3-javascriptcore.doHaddock = false; }
-          ];
-        };
+          test2 = final.haskell-nix.project' ( { lib, ... }: {
+            src = ./.;
+            compiler-nix-name = "ghc8107";
+            modules = [
+              { packages.webkit2gtk3-javascriptcore.doHaddock = false; }
+            ];
+
+            shell = {
+              name = "dev-shell";
+              packages = ps: [ ps.test2 ];
+
+              crossPlatforms = p: [ p.ghcjs ];
+
+              meta.platforms = lib.platforms.unix;
+            };
+          });
 	      }) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        flake = pkgs.test2.flake {};
+        flake = pkgs.test2.flake {
+              crossPlatforms = p: with p; [ ghcjs ];
+        };
       in
         flake // {
           defaultPackage = flake.packages."test2:exe:test2";
